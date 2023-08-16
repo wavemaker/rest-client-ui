9dc9702ba6366c36851bd314209e0e89a78a1a3e
Ponnarasi <ponnarasi.a@techconative.com>
2023-08-16 18:04:42 +0530

    Changes

--- examples/angular-app/src/app/modal/modal.component.ts
+++ examples/angular-app/src/app/modal/modal.component.ts
@@ -11,7 +11,15 @@ export class ModalComponent implements OnInit{
   ngOnInit(): void {
     const reactUI = RestImport({
           dom_id: '#rest-import-ui-modal',
-          
+          proxy_conf :{
+            'get-default-provider': '',
+            "getprovider":"",
+          },
+          default_proxy_state: "ON",
+          oAuthConfig:{
+            'get-default-provider': 'https://www.wavemakeronline.com/studio/services/oauth2/providers/default',
+            "getprovider":"https://www.wavemakeronline.com/studio/services/projects/WMPRJ2c91808888f5252401896880222516b1/oauth2/providers",
+          }
         });
   }
  
