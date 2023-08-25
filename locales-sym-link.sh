aec9bdb519a2e724cbecdf081f21d061357e0c30
Vivek Dhayalan <dvvivek@gmail.com>
2023-08-25 15:40:58 +0530

    update scripts

--- locales-sym-link.sh
+++ locales-sym-link.sh
@@ -1,9 +1,10 @@
-mk -p src/jsx/locales
+md -p src/jsx/locales
 cd src/jsx/locales
-New-Item -Path ./da.json -ItemType SymbolicLink -Value ../../locales/da.json
-New-Item -Path ./de.json -ItemType SymbolicLink -Value ../../locales/de.json
-New-Item -Path ./en.json -ItemType SymbolicLink -Value ../../locales/en.json
-New-Item -Path ./es.json -ItemType SymbolicLink -Value ../../locales/es.json
-New-Item -Path ./fr.json -ItemType SymbolicLink -Value ../../locales/fr.json
+
+ln -s ../../../locales/da.json da.json
+ln -s ../../../locales/de.json de.json
+ln -s ../../../locales/en.json en.json
+ln -s ../../../locales/es.json es.json
+ln -s ../../../locales/fr.json fr.json
 
 cd ../../../
